ef5549157c7754ee73be48c244f15cb8255ee657
saurs saurav <saurssaurav33@gmail.com>
2017-01-09 12:41:26 +0545

    second init

---
+++ comment/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace isaurssaurav\comment;
+
+/**
+ * comment module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'isaurssaurav\comment\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
